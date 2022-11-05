5651ff73cdb80d24b5db4a35f74c9f030d3040e4
GeorgeKarlinzer <georg11.01@mail.ru>
2022-11-05 10:18:01 +0100

    Change skills.json

--- Scripts/ContentGenerator.ts
+++ Scripts/ContentGenerator.ts
@@ -1,4 +1,8 @@
 
-export function generateSkills() {
-
+export default async function generateSkills() {
+    var response = await fetch('https://raw.githack.com/GeorgeKarlinzer/CV-Content/main/Data/skills.json');
+    var skills = await response.json()
+    console.log(skills);
 }
+
+console.log('asd');
